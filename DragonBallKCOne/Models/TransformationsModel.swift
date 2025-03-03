d8e30835a0ad34bf94720328cc0b9eaf3e6f34eb
Victoria Saldaña <victoria1236@gmail.com>
2025-03-02 23:51:53 -0300

    Create newmodel for transformation

---
+++ DragonBallKCOne/Models/TransformationsModel.swift
@@ -0,0 +1,16 @@
+//
+//  TransformationsModel.swift
+//  DragonBallKCOne
+//
+//  Created by Natacha Saldaña on 02-03-25.
+//
+
+import Foundation
+
+
+struct TransformationsModel: Codable {
+    let id: String
+    let name: String
+    let description: String
+    let photo: String
+}
